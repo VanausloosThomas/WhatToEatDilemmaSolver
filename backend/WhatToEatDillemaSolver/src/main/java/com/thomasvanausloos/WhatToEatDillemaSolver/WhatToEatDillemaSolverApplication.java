9651de9c56430429e2bceb2b30d5482ae190397a
VanausloosThomas <thomas.vanausloos@cegeka.com git config --global core.autocrlf inputgit config --global user.email thomas.vanausloos@cegeka.com>
2017-09-16 21:10:24 +0200

    added spring boot backend

---
+++ backend/WhatToEatDillemaSolver/src/main/java/com/thomasvanausloos/WhatToEatDillemaSolver/WhatToEatDillemaSolverApplication.java
@@ -0,0 +1,12 @@
+package com.thomasvanausloos.WhatToEatDillemaSolver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToEatDillemaSolverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToEatDillemaSolverApplication.class, args);
+	}
+}
